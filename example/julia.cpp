7ff4785c9599ef04d0f8ef6cc6ae635af7f3add7
Martin Taylor <martin6taylor@gmail.com>
2020-12-12 18:05:33 +0800

    bring more key operations for julia

--- example/julia.cpp
+++ example/julia.cpp
@@ -15,8 +15,7 @@ public:
 	vec2f center;
 	float scale;
 	float aspectRatio;
-	bool animate;
-	bool invalidate;
+	bool  dirty;
 public:
 	JuliaParameters();
 	
@@ -27,90 +26,101 @@ JuliaParameters::JuliaParameters():
 		constant(-0.662, 0.343),  // value can be tweaked!
 		center(0, 0),
 		scale(1.0),
-		animate(false),
-		invalidate(true)
+		dirty(true)
 {
 }
 
 std::string JuliaParameters::toString() const
 {
 	std::ostringstream oss;
-	oss << "c=(" << constant.x << ", " << constant.y << "), "
-			<< "offset=(" << center.x << ", " << center.y << "), "
-			<< "scale=" << scale;
+	oss << "constant" << '=' << '(' << constant.x << ", " << constant.y << ')' << ", "
+			<< "offset" << '=' << '(' << center.x << ", " << center.y << ')' << ", "
+			<< "scale" << '=' << scale;
 	return oss.str();
+}
+
+inline void setJuliaParameters(GLFWwindow* window, JuliaParameters* parameters)
+{
+	glfwSetWindowUserPointer(window, parameters);
+}
+
+inline JuliaParameters* getJuliaParameters(GLFWwindow* window)
+{
+	return static_cast<JuliaParameters*>(glfwGetWindowUserPointer(window));
 }
 
 static void onKey(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	std::ignore = window;
 	std::ignore = scancode;
 	std::ignore = mods;
+	if(action != GLFW_RELEASE)
+		return;
 	
-	JuliaParameters* parameters = static_cast<JuliaParameters*>(glfwGetWindowUserPointer(window));
+	JuliaParameters* parameters = getJuliaParameters(window);
 	assert(parameters);
+	parameters->dirty = true;
 	
 	bool isControlDown = glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
 			glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS;
 	bool isShiftDown = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ||
 			glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
-	float step;
-	if(isControlDown)
-		step = 0.1;
-	else if(isShiftDown)
-		step = 0.01;
-	else
-		step = 0.001;
+	float step = isControlDown? 0.1F: (isShiftDown? 0.01F: 0.001F);
 	
 	switch(key)
 	{
 	case GLFW_KEY_ESCAPE:
-		if(action == GLFW_PRESS)
-			glfwSetWindowShouldClose(window, GLFW_TRUE);
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 		break;
 	
-	case GLFW_KEY_A:
-		parameters->center.x -= step;
-		break;
-	case GLFW_KEY_D:
-		parameters->center.x += step;
-		break;
-	case GLFW_KEY_S:
-		parameters->center.y -= step;
-		break;
-	case GLFW_KEY_W:
-		parameters->center.y += step;
-		break;
-		
-	case GLFW_KEY_LEFT:
-		parameters->constant.x -= step;
-		break;
-	case GLFW_KEY_RIGHT:
-		parameters->constant.x += step;
-		break;
-	case GLFW_KEY_DOWN:
-		parameters->constant.y -= step;
-		break;
-	case GLFW_KEY_UP:
-		parameters->constant.y += step;
-		break;
+	case GLFW_KEY_A: parameters->center.x -= step; break;
+	case GLFW_KEY_D: parameters->center.x += step; break;
+	case GLFW_KEY_S: parameters->center.y -= step; break;
+	case GLFW_KEY_W: parameters->center.y += step; break;
+	
+	case GLFW_KEY_LEFT:  parameters->constant.x -= step; break;
+	case GLFW_KEY_RIGHT: parameters->constant.x += step; break;
+	case GLFW_KEY_DOWN:  parameters->constant.y -= step; break;
+	case GLFW_KEY_UP:    parameters->constant.y += step; break;
 
-	case GLFW_KEY_SPACE:
-		if(action == GLFW_RELEASE)
-			parameters->animate = !parameters->animate;
+	case GLFW_KEY_P:
+	{
+		std::string text = parameters->toString();
+		if(isShiftDown)
+			slog.d(TAG, "%s", text.c_str());
+		else
+		{
+			vec2i size = getWindowSize(window);
+			snapshot(text + ".png", size.width, size.height);
+		}
+	}
+		break;
+	
+	default:
+		// Only dirty when any keys above is striked.
+		parameters->dirty = false;
 		break;
 	}
-	
-	parameters->invalidate = true;
-//	slog.d(TAG, "%s", parameters->toString().c_str());
 }
 
-void onMouseScroll(GLFWwindow* window, double xoffset, double yoffset)
+vec2f calculatePosition(GLFWwindow* window, double x, double y)
 {
-	std::ignore = window;
+	double x0, y0;
+	glfwGetCursorPos(window, &x0, &y0);
+	int32_t width, height;
+	glfwGetWindowSize(window, &width, &height);
+	
+	vec2f position;
+	position.x = static_cast<float>(x0) / width;
+	position.y = 1.0F - static_cast<float>(y0) / height;
+	position = 2.0F * position - vec2f(1.0F, 1.0F);  // map [0, 1] to [-1, +1]
+	return position;
+}
+
+static void onMouseScroll(GLFWwindow* window, double xoffset, double yoffset)
+{
 	std::ignore = xoffset;
 
-	JuliaParameters* parameters = static_cast<JuliaParameters*>(glfwGetWindowUserPointer(window));
+	JuliaParameters* parameters = getJuliaParameters(window);
 	assert(parameters);
 	
 	bool isControlDown = glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
@@ -118,43 +128,64 @@ void onMouseScroll(GLFWwindow* window, double xoffset, double yoffset)
 	float factor = isControlDown? std::sqrt(2.0): std::pow(2, 1.0 / 12);
 	float scaleFactor = yoffset > 0? factor: 1 / factor;
 	parameters->scale *= scaleFactor;
-#if 1
-	vec2i mousePosition = getCursorPosition(window);
-	vec2i windowSize = getWindowSize(window);
-	vec2f position;  // [-aspectRatio, +aspectRatio] x [-1, +1]
-	position.x = static_cast<float>(mousePosition.x) / windowSize.width;
-	position.y = 1.0 - static_cast<float>(mousePosition.y) / windowSize.height;
-	position = 2.0F * position - vec2f(1.0F, 1.0F);  // map [0, 1] to [-1, +1]
-	position.x *= parameters->aspectRatio;  // with respect to aspect ratio
+
+	vec2f position = calculatePosition(window, xoffset, yoffset);
 	
-	// scale with respect to mouse position
+	// scale with respect to the current mouse position
 	parameters->center = (parameters->center - position) * scaleFactor + position;
-#endif
 	
-	parameters->invalidate = true;
+	parameters->dirty = true;
 //	slog.d(TAG, "yoffset=%f, scaleFactor=%f, scale=%f", yoffset, scaleFactor, parameters->scale);
+}
+
+static vec2f downPosition;
+static vec2f downCenter;
+static void onMouse(GLFWwindow* window, int button, int action, int mods)
+{
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		double downX, downY;
+		glfwGetCursorPos(window, &downX, &downY);
+		downPosition = calculatePosition(window, downX, downY);
+		
+		downCenter = getJuliaParameters(window)->center;
+	}
+}
+
+static void onCursorMove(GLFWwindow* window, double x, double y)
+{
+	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+	if(state == GLFW_PRESS)
+	{
+		vec2f movedPosition = calculatePosition(window, x, y);
+		JuliaParameters* parameters = getJuliaParameters(window);
+		parameters->center = downCenter - (movedPosition - downPosition) * parameters->scale;
+		parameters->dirty = true;
+	}
 }
 
 static void onWindowResize(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 	
-	JuliaParameters* parameters = static_cast<JuliaParameters*>(glfwGetWindowUserPointer(window));
+	JuliaParameters* parameters = getJuliaParameters(window);
 	assert(parameters);
 	parameters->aspectRatio = static_cast<float>(width) / height;
-	parameters->invalidate = true;
+	parameters->dirty = true;
 }
 
 int main()
 {
-	const char* usage = R""(
-Julia fractal: z = z^2 + constant
+	const char* usage = R""(Julia fractal: z = z^2 + C
 
 Usage:
-- use direction keys to tweak constant, left/right for X value, down/up for Y value.
-- use ADSW keys to adjust window of the complex plane.
-- combined with Ctrl key, you get bigger step.
-- MMB key zoom in/out the window to a fine-grain extend.
+- Use direction keys to tweak constant C, left/right for X value, down/up for Y value.
+- Use ADSW keys to move window of the complex plane.
+- Combined with Shift key, you get big step.
+- Combined with Control key, you get bigger step. When Control and Shift are down, Control prevails.
+- Use MMB key to zoom in/out the window to a fine-grain extend.
+- move mouse with LMB down to window of the complex plane.
+- Use P key to snapshot current frame, Shift + P prints current parameters.
 )"";
 	printf("%s\n", usage);
 	
@@ -183,14 +214,24 @@ Usage:
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, ::onWindowResize);
 	glfwSetKeyCallback(window, onKey);
+	glfwSetMouseButtonCallback(window, onMouse);
+	glfwSetCursorPosCallback(window, onCursorMove);
 	glfwSetScrollCallback(window, onMouseScroll);
 
 	loadGL();
 	GL::enableDebugMessage();
+	glDisable(GL_DEPTH_TEST);
 	
 	JuliaParameters parameters;
 	parameters.aspectRatio = static_cast<float>(windowWidth) / windowHeight;
-	glfwSetWindowUserPointer(window, &parameters);
+	setJuliaParameters(window, &parameters);
+/*
+	        point: xy
+	1--------0  0: ++
+	|        |  1: -+
+	|        |  2: --
+	2--------3  3: +-
+*/
 	std::string vertexShaderSource = ShaderFactory::VERSION + R""(
 uniform vec2 center;
 uniform float scale;
@@ -200,14 +241,23 @@ noperspective out vec2 z0;
 
 void main()
 {
-	const vec2 vertices[4] = vec2[4](
-			vec2(+aspectRatio, +1.0),
-			vec2(-aspectRatio, +1.0),
-			vec2(-aspectRatio, -1.0),
-			vec2(+aspectRatio, -1.0)
-	);
+	vec2 vertex;
+	if(aspectRatio >= 1.0)
+		vertex = vec2(aspectRatio, 1.0);
+	else
+		vertex = vec2(1.0, 1.0 / aspectRatio);
 	
+#if 0
+	const vec2 vertices[4] = vec2[4](
+			vec2(+vertex.x, +vertex.y),
+			vec2(-vertex.x, +vertex.y),
+			vec2(-vertex.x, -vertex.y),
+			vec2(+vertex.x, -vertex.y));
 	vec2 vertex = vertices[gl_VertexID];
+#else
+	float x = 1.5 - float(gl_VertexID);
+	vertex *= sign(vec2(abs(x) - 1.0, x));
+#endif
 	z0 = center + vertex * scale;
 	gl_Position = vec4(vertex, 0.5, 1.0);
 }
@@ -244,20 +294,16 @@ void main()
 	else
 	{
 		float x = float(iteration) / float(maxIteration);
-//		color = vec4(x, (z.x + 1) * 0.5, (z.y + 1) * 0.5, 1);
 		color = vec4(texture(colorMap, x).rgb, 1.0);
 	}
 }
 )"";
 
-	uint32_t vertexShader   = ShaderFactory::loadShader(Shader::VERTEX_SHADER, vertexShaderSource);
-	uint32_t fragmentShader = ShaderFactory::loadShader(Shader::FRAGMENT_SHADER, fragmentShaderSource);
-	Program program(Program::createProgram(2, vertexShader, fragmentShader));
+	Program program(vertexShaderSource, fragmentShaderSource);
 	slog.i(TAG, "%s", program.getActiveVariables().c_str());
-	
-	int32_t centerLocation   = program.getUniformLocation("center");
-	int32_t scaleLocation     = program.getUniformLocation("scale");
-	int32_t constantLocation = program.getUniformLocation("constant");
+	int32_t centerLocation      = program.getUniformLocation("center");
+	int32_t scaleLocation       = program.getUniformLocation("scale");
+	int32_t constantLocation    = program.getUniformLocation("constant");
 	int32_t aspectRatioLocation = program.getUniformLocation("aspectRatio");
 	
 	Texture texture = createThermosTexture(256);
@@ -270,28 +316,22 @@ void main()
 
 	while(!glfwWindowShouldClose(window))
 	{
-		if(parameters.invalidate || parameters.animate)
+		if(parameters.dirty)
 		{
-			// clear default frame buffer
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-			int64_t time = getCurrentTime();
-			float t = static_cast<float>(time) * 1E-3;
-			vec2f constant = parameters.animate? vec2f(std::cos(2.1f * t + 0.23), std::sin(4.8f * t - 0.07)): parameters.constant;
-			
 			program.use();
 			Program::setUniform(centerLocation, parameters.center);
 			Program::setUniform(scaleLocation, parameters.scale);
-			Program::setUniform(constantLocation, constant);
+			Program::setUniform(constantLocation, parameters.constant);
 			Program::setUniform(aspectRatioLocation, parameters.aspectRatio);
 			
+			glClear(GL_COLOR_BUFFER_BIT);
 			glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
 			glfwSwapBuffers(window);
 			
-			parameters.invalidate = false;
+			parameters.dirty = false;
 		}
-		glfwPollEvents();
+		
+		glfwWaitEvents();
 	}
 	
 	glDeleteVertexArrays(1, &vao);
